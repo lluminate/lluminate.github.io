ad0ec82d936127936955618d4e5b25044c640425
Victor <victoryang2005@outlook.com>
2023-04-22 18:27:00 -0700

    fixed javascript

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const trailer = document.getElementById("trailer");
+window.onload = () => {
+  const trailer = document.getElementById("trailer");
 
-window.onmousemove = e => {
-  const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-    
-  trailer.style.transform = `translate(${x}px, ${y}px)`;
+  window.onmousemove = e => {
+    const x = e.clientX - trailer.offsetWidth / 2,
+          y = e.clientY - trailer.offsetHeight / 2;
+
+    const keyframes = {
+      transform: `translate(${x}px, ${y}px)`
+    }
+
+    trailer.animate(keyframes, {
+      duration: 800,
+      fill: "forwards",
+    });
+  }
 }
 
-function myFunction(){
-  document.getElementById("paragraph").innerHTML = "I am a student at Burnaby South"
+function contact(){
+  document.getElementById("contact").style.display = "flex";
+  document.getElementById("contactButton").style.display = "none";
 }
