0843bec716b28686990b103bd3aca357b0aff6ee
Victor <victoryang2005@outlook.com>
2023-04-22 17:21:09 -0700

    changed js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const trailer = document.getElementById("trailer");
 window.onmousemove = e => {
   const x = e.clientX - trailer.offsetWidth / 2,
         y = e.clientY - trailer.offsetHeight / 2;
-
-  const keyframes = {
-    transform: `translate(${x}px, ${y}px)`
-  }
-
-  triler.animate(keyframes, {
-    duration: 1000,
-    easing: "ease-in-out"
-    });
+    
+  trailer.style.transform = `translate(${x}px, ${y}px)`;
 }
