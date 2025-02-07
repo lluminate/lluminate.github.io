cc13e4924571f652670d34e2144126520f531600
Victor <victoryang2005@outlook.com>
2025-02-07 10:21:02 -0800

    Transition to React

--- index.js
+++
@@ -1,7 +0,0 @@
-function toggleContact() {
-  const contactButton = document.getElementById("contact");
-  const isClicked = contactButton.getAttribute("data-clicked") === "true";
-  const newButtonText = isClicked ? "Contact" : "Github: @lluminate";
-  contactButton.innerHTML = newButtonText;
-  contactButton.setAttribute("data-clicked", !isClicked);
-}
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hi! My name is <span class="gradientText">Victor</span>
+      </h1>
+      <p>I am a student</p>
+    </div>
+  );
+}
+
+export default App;
