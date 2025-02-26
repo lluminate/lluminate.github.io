1ae8bc4ef8fe146292452529cd02108b73b0bba9
Victor <victoryang2005@outlook.com>
2025-02-26 00:39:00 -0800

    Migrate to next.js, typescript, and tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="bg-[rgb(20,20,20)] text-[white] h-screen flex flex-col justify-center items-center text-center select-none m-0">
+      <h1 className="text-3xl font-bold mb-4">
+        Hi! My name is <span className="bg-clip-text whitespace-nowrap gradient-text">Victor</span>
+      </h1>
+      <p className="text-2xl">I am a student</p>
+    </div>
+  );
+}
