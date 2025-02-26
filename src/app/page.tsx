2bcc50e7612a5ee87b862d6b1349a89dad03cb68
Victor <victoryang2005@outlook.com>
2025-02-26 10:07:36 -0800

    create github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <div className="bg-[rgb(20,20,20)] text-[white] h-screen flex flex-col justify-center items-center text-center select-none m-0">
-      <h1 className="text-3xl font-bold mb-4">
+      <h1 className="text-3xl font-bold mb-4 font-poppins">
         Hi! My name is <span className="bg-clip-text whitespace-nowrap gradient-text">Victor</span>
       </h1>
-      <p className="text-2xl">I am a student</p>
+      <p className="text-2xl font-karla">I am a student</p>
     </div>
   );
 }
