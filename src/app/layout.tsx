134c56a4fb7ba93de6d63f0bb3f56140a10ecb6e
Victor <victoryang2005@outlook.com>
2025-05-17 00:47:20 -0700

    Working Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import type {ReactNode} from "react";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {ThemeProvider} from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: "Victor's Website",
-  description: "Victor's Portfolio",
+    title: "Victor Yang",
+    description: "Victor's Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+interface RootLayoutProps {
+    children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body>
+            <ThemeProvider
+                attribute={"class"}
+                defaultTheme={"dark"}
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+        </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,60 @@
+import {Mail, ExternalLink, FileText, Heart} from "lucide-react"
+import ThemeToggle from "@/components/theme-toggle";
+import Noise from "@/components/noise";
+import {Card, CardContent} from "@/components/ui/card";
+
 export default function Home() {
-  return (
-    <div className="bg-[rgb(20,20,20)] text-[white] h-screen flex flex-col justify-center items-center text-center select-none m-0">
-      <h1 className="text-3xl font-bold mb-4 font-poppins">
-        Hi! My name is <span className="bg-clip-text whitespace-nowrap gradient-text">Victor</span>
-      </h1>
-      <p className="text-2xl font-karla">I am a student</p>
-    </div>
-  );
+    return (
+        <div className={"px-6 py-20 min-h-svh flex flex-col justify-center bg-background text-foreground"}>
+            <div className={"fixed inset-0 pointer-events-none w-full h-full z-100"}>
+                <Noise
+                    patternSize={250}
+                    patternScaleX={1}
+                    patternScaleY={1}
+                    patternRefreshInterval={2}
+                    patternAlpha={15}
+                />
+            </div>
+            <div className="fixed top-4 right-4">
+                <ThemeToggle />
+            </div>
+            <div className={"w-full max-w-fit mx-auto"}>
+                <div className={"flex flex-col gap-6"}>
+                    <Card className={"shadow-2xl rounded-3xl border-transparent dark:border-border/64"}>
+                        <CardContent className={"p-10 m-3"}>
+                            <div className={"font-serif"}>
+                                <h1 className={"font-bold text-2xl mb-3"}>Victor Yang.</h1>
+                                <p className={"text-balance"}>
+                                    Get in touch via&nbsp;
+                                    <a target={"_blank"} rel={"noopener noreferrer"} href={"https://www.linkedin.com/in/victor-y"}
+                                       className={"underline"}>
+                                        Linkedin <ExternalLink size={"1.1em"} className={"inline align-text-top"}/>
+                                    </a>
+                                    &nbsp;or&nbsp;
+                                    <a href={"mailto:victoryang2005@outlook.com"} className={"underline"}>
+                                        Email <Mail size={"1.1em"} className={"inline align-text-top"}/>
+                                    </a>
+                                    , browse my projects on&nbsp;
+                                    <a target={"_blank"} rel={"noopener noreferrer"} href={"https://www.github.com/lluminate"}
+                                       className={"underline"}>
+                                        Github <ExternalLink size={"1.1em"} className={"inline align-text-top"}/>
+                                    </a>
+                                    &nbsp;and view my&nbsp;
+                                    <a target={"_blank"} rel={"noopener noreferrer"} href={"/May_2025_Victor_Yang_Resume.pdf"}
+                                       className={"underline"}>
+                                        Resume <FileText size={"1.1em"} className={"inline align-text-top"}/>
+                                    </a>.
+                                </p>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+            <footer className={"bottom-0"}>
+                <div className={"flex flex-col gap-2 items-center justify-center mt-10"}>
+                    <p className={"text-sm text-muted-foreground text-center"}>© 2025 Victor Yang. Made with <Heart strokeWidth={0} size={"1em"} className={"inline align-text-bottom fill-current"}/> in Vancouver. </p>
+                </div>
+            </footer>
+        </div>
+    );
 }
